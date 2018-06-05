037b3d67dac3f305a24a11b58c9c61ad3f8f701c
LzyRapx <LzyRapx@gmail.com>
2018-06-05 20:29:27 +0800

    Create 53. Maximum Subarray（最大子段和）.cpp

---
+++ LeetCode/Dynamic Programming/Easy/53. Maximum Subarray（最大子段和）.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int dp = 0;
+        int ans = -INT_MAX;
+        for(int i = 0;i < nums.size();i++) {
+            if(dp >= 0){
+                dp += nums[i];
+            }
+            else dp = nums[i];
+            if(ans < dp) {
+                ans = dp;
+            }
+        }
+        return ans;
+    }
+};
