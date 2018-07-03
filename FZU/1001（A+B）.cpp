801fe42e2bc5337797658bc9efdb8b98c15a46c6
LzyRapx <LzyRapx@gmail.com>
2018-07-03 11:23:07 +0800

    A+B

---
+++ FZU/1001（A+B）.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+typedef long long ll;
+const int maxn=1e6+10;
+ll a[10],b[10],c[10];
+int main()
+{
+   int n,m;
+   while(cin>>n>>m)
+   {
+   	cout<<n+m<<endl;
+   }
+    return 0;
+}
