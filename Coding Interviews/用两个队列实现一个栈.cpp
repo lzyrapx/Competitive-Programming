75f8e2e626cb71e3d7b0354a10476a07db32caa1
LzyRapx <LzyRapx@gmail.com>
2018-08-31 03:14:04 +0800

    update

--- Coding Interviews/用两个队列实现一个栈.cpp
+++ Coding Interviews/用两个队列实现一个栈.cpp
@@ -1,5 +1,48 @@
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    MyStack() {
+        
+    }
+    queue<int>q;
+    queue<int>tmp;
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+       while(!q.empty()) {
+           tmp.push(q.front());
+           q.pop();
+       }
+        q.push(x);
+        while(!tmp.empty()) {
+            q.push(tmp.front());
+            tmp.pop();
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return q.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q.empty();
+    }
+};
 
-入栈：将元素进队列A
-出栈：判断队列A中元素的个数是否为1，如果等于1，则出队列，否则将队列A中的元素   
-以此出队列并放入队列B，直到队列A中的元素留下一个，然后队列A出队列，再把   
-队列B中的元素出队列以此放入队列A中。
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * bool param_4 = obj.empty();
+ */
