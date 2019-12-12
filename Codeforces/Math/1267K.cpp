a080afc7c56dec45887002777f8e93337536ca34
LzyRapx <1303377506@qq.com>
2019-12-12 21:18:48 +0800

    update 1267K

--- Codeforces/Math/1267K.cpp
+++ Codeforces/Math/1267K.cpp
@@ -17,9 +17,11 @@ typedef long long ll;
 ll fac[23];
 int cnt[25];
 
-vector<int>calc(ll x) {
-    vector<int>res;
-    for(int i = 2; x; i++) {
+vector<int> calc(ll x)
+{
+    vector<int> res;
+    for (int i = 2; x; i++)
+    {
         res.push_back(x % i);
         x /= i;
     }
@@ -29,40 +31,49 @@ vector<int>calc(ll x) {
 int main(int argc, char const *argv[])
 {
     fac[0] = 1;
-    for(int i = 1; i <= 20; i++) {
+    for (int i = 1; i <= 20; i++)
+    {
         fac[i] = fac[i - 1] * i;
     }
     int t;
     cin >> t;
-    while(t--) {
+    while (t--)
+    {
         ll x;
         cin >> x;
         ll ans = 0;
-        vector<int>rem = calc(x);
-        for(int i = 0; i < rem.size(); i++) {
-            if(rem[i]) {
+        vector<int> rem = calc(x);
+        for (int i = 0; i < rem.size(); i++)
+        {
+            if (rem[i])
+            {
                 ll sum = 1;
-                vector<int>ve;
-                for(int j = 0; j < rem.size(); j++) {
-                    if(i != j) {
+                vector<int> ve;
+                for (int j = 0; j < rem.size(); j++)
+                {
+                    if (i != j)
+                    {
                         ve.push_back(rem[j]);
                     }
                 }
-                for(int j = ve.size() - 1; j >= 0; --j) {
+                for (int j = 0; j < ve.size(); j++)
+                {
+                    cerr << "a = " << max(0, j + 2 - max(1, ve[j])) << " " << ve[j] << endl;
                     sum *= max(0, j + 2 - max(1, ve[j]));
                 }
                 ans += sum;
             }
         }
         memset(cnt, 0, sizeof(cnt));
-        for(int i = 0; i < rem.size(); i++) {
+        for (int i = 0; i < rem.size(); i++)
+        {
             cnt[rem[i]]++;
         }
-        for(int i = 0; i < rem.size(); i++) {
+        for (int i = 0; i < rem.size(); i++)
+        {
             ans /= fac[cnt[i]];
         }
         cout << ans - 1 << endl;
     }
     return 0;
 }
-
